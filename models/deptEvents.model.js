d9ddb46f9d2f6f1908847f2b322de2513ab7abf6
Ananya Singh <ananyasingth123@gmail.com>
2025-03-05 22:59:10 +0530

    updated database for dept section

--- models/deptEvents.model.js
+++ models/deptEvents.model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const deptEventSchema=new Schema({
     year:String,
-    description:String,
+    description:[String],
     department:{
         type:String,
         enum: ["bs", "cse", "ece"],
