6d9b79b4917e81ed8f4abdbf591ba51aa6dd55a1
Ananya Singh <ananyasingth123@gmail.com>
2025-03-09 19:48:42 +0530

    few changes

--- app.js
+++ app.js
@@ -121,19 +121,17 @@ app.get("/aboutUs", (req, res) => {
   res.render("aboutUs");
 });
 
-// // Basic Science Routes
-// app.get("/department", (req, res) => {
-//   res.redirect("/basic_science/aboutDepartment");
-// });
-// app.get("/basic_science/:page", (req, res) => {
-//   const { page } = req.params;
-//   res.render(`basic_science/${page}`);
-// });
-// app.get("/department", (req, res) => {
-//   res.redirect("/basic_science/aboutDepartment");
-// });
+// Basic Science Routes
 app.get("/department", (req, res) => {
-  res.render("department");
+  res.redirect("/basic_science/aboutDepartment");
+});
+app.get("/basic_science/:page", (req, res) => {
+  const { page } = req.params;
+  res.render(`basic_science/${page}`);
+});
+
+app.get("/placements", (req, res) => {
+  res.redirect("/placements/about-us");
 });
 
 app.get("/placements/:page", (req, res) => {
